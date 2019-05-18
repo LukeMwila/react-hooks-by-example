a0fff5a17f36b7b52c2e8ab7cf91d0dccef80fc5
Luke Mwila <outlier.developer@gmail.com>
2019-05-18 17:47:42 +0200

    feat: Added useReducer to manage the state of the toDoList value

--- src/containers/ToDo.tsx
+++ src/containers/ToDo.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Button, Form } from "reactstrap";
+import uuid from "uuid";
 
 /** Presentation */
 import ErrorMessage from "../components/ErrorMessage";
@@ -7,17 +8,56 @@ import ErrorMessage from "../components/ErrorMessage";
 /** Custom Hooks */
 import useErrorHandler from "../utils/custom-hooks/ErrorHandler";
 
+enum ActionType {
+  add = "ADD",
+  delete = "DELETE"
+}
+
+interface IState {
+  toDoList: Array<{ id: string; toDo?: string }>;
+}
+
+interface IAction {
+  type: ActionType;
+  payload: {
+    id: string;
+    toDo?: string;
+  };
+}
+
+const initialState: IState = { toDoList: [] };
+
+const reducer: React.Reducer<IState, IAction> = (state, action) => {
+  switch (action.type) {
+    case ActionType.add:
+      return {
+        toDoList: [...state.toDoList, action.payload]
+      };
+    case ActionType.delete:
+      return {
+        toDoList: state.toDoList.filter(toDo => toDo.id !== action.payload.id)
+      };
+    default:
+      throw new Error();
+  }
+};
+
 const ToDo: React.FC<{}> = () => {
+  const [toDoList, dispatch] = React.useReducer(reducer, initialState);
   const { error, showError } = useErrorHandler(null);
   const textInput = React.useRef<HTMLInputElement>(null);
 
   const addNewToDoItem = () => {
     if (textInput.current) {
-      const todo = textInput.current.value;
+      const toDo = textInput.current.value;
+      dispatch({ type: ActionType.add, payload: { id: uuid(), toDo } });
+      textInput.current.value = "";
     } else {
       showError("Please type an item before clicking add.");
     }
   };
+
+  console.log("To do list:", toDoList);
 
   return (
     <Form
